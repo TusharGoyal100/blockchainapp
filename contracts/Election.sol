4188dd40428050c89a53d5bf2547a7c2c6ebfd85
tushar <tushargoyal100@gmail.com>
2022-03-01 11:24:51 +0530

    adding ui and rendering client side code

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,9 +3,26 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract Election {
     
-    string public candidate;
-     
+    //string public candidate;
+
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
     constructor()  public{
-        candidate="Candidate 1";
+        //candidate="Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount]=Candidate(candidatesCount,_name,0);
     }
 }
